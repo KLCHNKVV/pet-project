72bcbcb479ba980a5ce0dae1d42e2f9b1e41c3d3
klchnkvv <daniel.kliuchnikov@mail.ru>
2017-04-29 21:52:44 +0300

    Some routes fixes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @page = 'Page'
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select 'title', 'Home Page'
+    assert_select 'title', "Home #{@page}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select 'title', 'Help Page'
+    assert_select 'title', "Help #{@page}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', 'About Page'
+    assert_select 'title', "About #{@page}"
   end
 
 end
