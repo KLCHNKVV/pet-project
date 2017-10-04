88001b40359e865c03e432f3c99b1d6150373197
klchnkvv <daniel.kliuchnikov@mail.ru>
2017-10-04 10:45:11 +0300

    Finish layout and add some tests

--- test/integration/fill_layout_test.rb
+++ test/integration/fill_layout_test.rb
@@ -3,15 +3,17 @@ require 'test_helper'
 class FillLayoutTest < ActionDispatch::IntegrationTest
 
   test 'valid links' do
-
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', signup_path
 
+    get signup_path
+    assert_select 'title', full_title('User Signup')
   end
 
 end
+
