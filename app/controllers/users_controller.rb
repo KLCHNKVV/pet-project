5cf04a04af9ecfac2f775e787489cd1edf8f1ae4
klchnkvv <daniel.kliuchnikov@mail.ru>
2017-10-06 16:58:14 +0300

    Finish sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,20 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
   end
 
+
+
   def create
-    @user = User.new params[:user]
+    @user = User.new user_params
     if @user.save
+      flash[:success] = 'Your profile was successfully created!'
+      redirect_to @user
     else
       render 'new'
     end
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
